95f61eb1e3906e3fbd6f26d763dc2293d4f8dcf7
amxson <91834366+amxson@users.noreply.github.com>
2022-06-04 18:18:27 +0300

    Update 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int y;
+
+for (y = 97; y <= 122; y++)
+{
+if (y == 101 || y == 113) continue;
+else
+{
+putchar(y);
+}
+}
+putchar('\n');
+return (0);
+}
