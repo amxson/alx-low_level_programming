c115574e995f9701d7dd3ef526ab5369dca190c0
amxson <91834366+amxson@users.noreply.github.com>
2022-06-04 18:54:13 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,35 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
 /**
- * main - Entry point
+ * main - main - two pairs of numbers that do not repeat
  *
- * Return: Always 0 (Success)
+ * Return (0)
  */
+
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
+	int i, j;
 
-	while (p < 58)
+	for (j = 0; j <= 99; j++)
 	{
-		while (q < 58)
+		for (i = j; i <= 99; i++)
 		{
-			while (y < 58)
+			if (i != j)
 			{
-				while (x < 58)
-				{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					x++;
-				}
-				y++;
-				x = 48;
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+				putchar(' ');
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
 			}
-			q++;
-			q = p;
-			x = q + 1;
+			if (j * 100 + i != 9899)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
 	}
 	putchar('\n');
+
 	return (0);
 }
